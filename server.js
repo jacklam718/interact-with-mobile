0b906b3f28088f83ca2898a6f168c9685b77409f
jack lam <jacklam718@gmail.com>
2015-09-02 02:32:22 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+var serveStatic = require('serve-static');
+var express = require('express');
+var ws = require('nodejs-websocket');
+var path = require('path');
+
+var app = express();
+
+// set static files
+app.use(serveStatic(__dirname + '/'));
+app.use(serveStatic(__dirname + '/vendor'));
+app.use(serveStatic(__dirname + '/public'));
+
+app.use('/', function(req, res, next) {
+  var url = /[a-z]+/.exec(req.url);
+  if (url === null) {
+    res.sendFile(path.join(__dirname, './index.html'));
+  }
+  next();
+});
+
+app.use('/client', function(req, res, next) {
+  console.log('/client');
+  var url = /[a-z]+/.exec(req.url)[0];
+
+  switch (url) {
+    case 'orientation':
+      res.sendFile(path.join(__dirname, 'public/client/orientation/orientation.html'));
+      break;
+  }
+});
+
+
+app.use('/server', function(req, res, next) {
+  var url = /[a-z]+/.exec(req.url)[0];
+
+  switch (url) {
+    case 'orientation':
+      res.sendFile(path.join(__dirname, 'public/server/orientation/orientation.html'));
+      break;
+  }
+})
+
+var server = ws.createServer(function(conn) {
+  console.log('new connection');
+  conn.on('text', function (str) {
+    console.log('Received ' + str);
+    broadcast(str);
+  });
+
+  conn.on('close', function(code, reason) {
+    console.log('Connection closed');
+  });
+})
+
+function broadcast(msg) {
+  server.connections.forEach(function(conn) {
+    conn.sendText(msg);
+  });
+}
+
+// express
+app.listen(8000, '0.0.0.0');
+// websocket
+server.listen(8001, '0.0.0.0');
+
+console.log('Start listen: 8000/8001');
